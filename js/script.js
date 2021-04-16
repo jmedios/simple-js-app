edb8d7af50bb7037dc628465555b8025e7200356
Jim Medios <jmedios@apple.com>
2021-04-15 23:00:21 -0700

    feat: add for and while loop to script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
 const pokemonList = [
     {
-    name: Bulbasaur,
-    height: 3,
+    name: 'Bulbasaur',
+    height: 0.7,
     types: ['grass', 'poison']
     },
     {
-    name: Squirtle,
+    name: 'Squirtle',
     height: 1,
     types: ['water']
     },
     {
-    name: Charmander,
+    name: 'Charmander',
     height: 2,
     types: ['fire']
     }
 ];
+
+// For loop that lists out the Pokemon's name and it's height
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ')
+}
+
+// While loop that will call out "Wow, that's big!" for Pokemon's w/ height >= 1
+i = 0;
+
+while (i < pokemonList.length) {
+// ternary test
+    pokemonList[i].height >= 1 ? document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!') : document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+    i++;
+
+} 
