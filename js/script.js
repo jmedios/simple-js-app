d5c04e8c2a0bacd95272abe2af5a0b77412fc1c1
Jim Medios <jmedios@gmail.com>
2021-04-27 22:55:27 -0700

    feat: add button event listener, button styling

--- js/script.js
+++ js/script.js
@@ -1,40 +1,69 @@
 const pokemonRepository = (function () {
-    
-    // Pokemon array
-    const pokemonList = [
-        {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass', 'poison']
-        },
-        {
-        name: 'Squirtle',
-        height: 1,
-        types: ['water']
-        },
-        {
-        name: 'Charmander',
-        height: 2,
-        types: ['fire']
-        }
-    ];
+  // Pokemon array
+  const pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["grass", "poison"],
+    },
+    {
+      name: "Squirtle",
+      height: 1,
+      types: ["water"],
+    },
+    {
+      name: "Charmander",
+      height: 2,
+      types: ["fire"],
+    },
+  ];
 
-    return {
-        getAll: function(){
-            return pokemonList;
-        },
+  return {
+    getAll: function () {
+      return pokemonList;
+    },
 
-        // Taking a stab at this. Not sure if it works.
-        add: function(pokemon){
-            typeof pokemon === 'object' && Object.keys(pokemon) === Object.keys(pokemonList) ? pokemonList.push(pokemon) : alert("Not an object.");
-        },
+    // Taking a stab at this. Not sure if it works.
+    add: function (pokemon) {
+      typeof pokemon === "object" &&
+      Object.keys(pokemon) === Object.keys(pokemonList)
+        ? pokemonList.push(pokemon)
+        : alert("Not an object.");
+    },
 
-        // Also trying this one out. Doesn't seem to work.
-        filter: function(query){
-            return pokemonList.filter(el => el.toLowerCase().indexOf(query.toLowerCase()) !== -1)
-        }
-    };
+    addListItem: function (pokemon) {
+      // selects the unordered list w/ class = pokemon-list
+      const pokeList = document.querySelector(".pokemon-list");
 
+      // creates list
+      const li = document.createElement("li");
+
+      // creates button
+      const button = document.createElement("button");
+
+      // button styling
+      button.innerText = pokemon.name;
+      button.classList.add("new-button");
+
+      // button behaviour
+      button.addEventListener("click", function () {
+        pokemonRepository.showDetails(pokemon);
+      });
+
+      // build list
+      li.appendChild(button);
+      pokeList.appendChild(li);
+    },
+
+    showDetails: function (pokemon) {
+      console.log(pokemon);
+    },
+
+    //     // Also trying this one out. Doesn't seem to work.
+    // filter: function(query){
+    //     return pokemonList.filter(el => el.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+    // }
+  };
 })();
 
 // creating new array test to duplicate pokemonList array. Didn't work.
@@ -51,12 +80,21 @@ const pokemonRepository = (function () {
 //     console.log(pokemon)
 // });
 
-pokemonRepository.getAll().forEach(pokemon => {
-    document.write(`${pokemon.name} (height: ${pokemon.height}) `)
+pokemonRepository.getAll().forEach((pokemon) => {
+  // document.write(`${pokemon.name} (height: ${pokemon.height}) `)
+  //   const pokemonList = document.querySelector(".pokemon-list");
+  //   const li = document.createElement("li");
+  //   const button = document.createElement("button");
+  //   button.innerText = pokemon.name;
+
+  //   button.classList.add("new-button");
+  //   li.appendChild(button);
+  //   pokemonList.appendChild(li);
+  pokemonRepository.addListItem(pokemon);
 });
 
 // The original while loop that was inside the IIFE
-   /*  // While loop that will call out "Wow, that's big!" for Pokemon's w/ height >= 1
+/*  // While loop that will call out "Wow, that's big!" for Pokemon's w/ height >= 1
     i = 0;
 
     while (i < pokemonList.length) {
